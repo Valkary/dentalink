8397381285b94ebd5b996565fc2cdc321e89a3d2
Pepe Salcedo <pepoclesng@gmail.com>
2022-01-04 13:52:02 -0600

    Terminando toda la funcionalidad esencial de la pagina de pacientes

--- components/ecosystems/Patients.jsx
+++ components/ecosystems/Patients.jsx
@@ -8,6 +8,7 @@ import DentureColors from "../atoms/DentureColors";
 import Denture from "../molecules/Denture";
 import ToothHistory from "../molecules/ToothHistory";
 import axios from "axios";
+import PatientExpedient from "../molecules/PatientExpedient";
 
 /*
   TODO:
@@ -17,6 +18,8 @@ import axios from "axios";
 
 const Patients = ({ userCreds }) => {
   const [showPatientsTable, setShowPatientsTable] = useState(true);
+  const [showPatientInfo, setShowPatientInfo] = useState(true);
+  const [showPatientDenture, setShowPatientDenture] = useState(true);
   const [patient, setPatient] = useState({});
 
   const [tooth, setTooth]  = useState({
@@ -49,7 +52,7 @@ const Patients = ({ userCreds }) => {
   return (
     <Grid
       templateColumns="75% 25%"
-      templateRows="[titulo] 5vh [lista-pacientes] auto [dentadura-paciente] auto"
+      templateRows="[titulo] 5vh [lista-pacientes] auto [informacion-paciente] auto [dentadura-paciente] auto"
       gap={2}
     >
       <GridItem
@@ -68,6 +71,11 @@ const Patients = ({ userCreds }) => {
         rowStart={2}
         rowEnd={3}
       >
+        <Flex pl="2rem" direction="row" justify="flex-start" align="center" width="100%" background="gray.100">
+          <strong style={{ paddingBottom: "0.1em" }}>Paciente:</strong>
+          <PatientSelector selectPatient={selectPatientFunc}></PatientSelector>
+        </Flex>
+
         <Flex pl="1rem" direction="row" justify="flex-start" align="center" width="100%" background="gray.100">
           <Button leftIcon={showPatientsTable ?  <IoIosArrowUp/> : <IoIosArrowDown/>} onClick={() => setShowPatientsTable(!showPatientsTable)}>
             {
@@ -76,11 +84,6 @@ const Patients = ({ userCreds }) => {
                 "Abrir tabla de pacientes"
             }
           </Button>
-        </Flex>
-
-        <Flex pl="2rem" direction="row" justify="flex-start" align="center" width="100%" background="gray.100">
-          <strong style={{ paddingBottom: "0.1em" }}>Paciente:</strong>
-          <PatientSelector selectPatient={selectPatientFunc}></PatientSelector>
         </Flex>
 
         {
@@ -96,33 +99,79 @@ const Patients = ({ userCreds }) => {
             <></>   
         }
       </GridItem>
-      <GridItem
-        colStart={1}
-        colEnd={3}
-        rowStart={3}
-        rowEnd={4}
-      >
-        <Flex pl="1rem" direction="row" justify="flex-start" align="center" width="100%" background="gray.100">
-          <IoIosArrowForward/>
-          Paciente
-        </Flex>
-        { 
-          patient.id ?
-            <>
-              <DentureColors></DentureColors>
-              <Denture selectTooth={selectToothFunc} patientID={patient.id}></Denture>
-              <Flex direction="column" justify="center" align="center" width="100%" height="100%">
-                <ToothHistory 
-                  toothName={tooth.tooth_name} 
-                  toothID={tooth.tooth_id} 
-                  patientID={patient.id}
-                  
-                ></ToothHistory> 
-              </Flex>
-            </> :
-            <></>
-          }
-      </GridItem>
+      
+      {
+        patient.id ?
+          <GridItem
+            colStart={1}
+            colEnd={3}
+            rowStart={3}
+            rowEnd={4}
+          >
+            <Flex pl="1rem" direction="row" justify="flex-start" align="center" width="100%" background="gray.100">
+              <Button leftIcon={showPatientInfo ?  <IoIosArrowUp/> : <IoIosArrowDown/>} onClick={() => setShowPatientInfo(!showPatientInfo)}>
+                {
+                  showPatientInfo ? 
+                    `Cerrar información del paciente: ${patient.names} ${patient.last_names}` :
+                    `Abrir información del paciente: ${patient.names} ${patient.last_names}`
+                }
+              </Button>
+            </Flex>
+
+            {
+              showPatientInfo ? 
+                <Flex 
+                  direction="column"            
+                  maxW="100%"
+                  maxH="100%"
+                >
+                  <PatientExpedient patient_data={patient}></PatientExpedient>
+                </Flex>
+                :
+                <></>   
+            }
+          </GridItem> :
+          <></>
+      }
+
+      {
+        patient.id ?
+          <GridItem
+            colStart={1}
+            colEnd={3}
+            rowStart={4}
+            rowEnd={5}
+          >
+            <Flex pl="1rem" direction="row" justify="flex-start" align="center" width="100%" background="gray.100">
+              <Button leftIcon={showPatientDenture ?  <IoIosArrowUp/> : <IoIosArrowDown/>} onClick={() => setShowPatientDenture(!showPatientDenture)}>
+                {
+                  showPatientDenture ? 
+                  `Mostrar dentadura del paciente: ${patient.names} ${patient.last_names}` :
+                  `No mostrar dentadura del paciente: ${patient.names} ${patient.last_names}`
+                }
+              </Button>
+            </Flex>
+
+            {
+              showPatientDenture ? 
+                <Flex direction="column">
+                  <DentureColors></DentureColors>
+                  <Denture selectTooth={selectToothFunc} patientID={patient.id}></Denture>
+                  <Flex direction="column" justify="center" align="center" width="100%" height="100%">
+                    <ToothHistory 
+                      toothName={tooth.tooth_name} 
+                      toothID={tooth.tooth_id} 
+                      patientID={patient.id}
+                      
+                    ></ToothHistory> 
+                  </Flex>
+                </Flex> :
+                <></>
+            }
+            
+          </GridItem> :
+          <></>
+      }
     </Grid>
   )
 }
